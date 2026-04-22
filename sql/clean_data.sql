18dfc0c60072198ec92f71aac258ce1420ae1d0e
alex-m-programmer <alexmallis.work@gmail.com>
2026-04-22 15:06:16 +0300

    feat: add advanced metrics (AOV, Spend per Customer) using CTEs

--- sql/clean_data.sql
+++ sql/clean_data.sql
@@ -1,16 +1,4 @@
-CREATE OR REPLACE TABLE 'retail-analytics-494014.raw_data.online_retail_cleaned' AS
-SELECT 
-  InvoiceNo,
-  StockCode,
-  Description,
-  Quantity,
-  InvoiceDate,
-  UnitPrice,
-  CustomerID,
-  Country
-FROM 
-  'retail-analytics-494014.raw_data.online_retail_raw'
-WHERE 
-  CustomerID IS NOT NULL 
-  AND Quantity > 0 
-  AND UnitPrice > 0;
+CREATE OR REPLACE TABLE `retail-analytics-494014.raw_data.online_retail_cleaned` AS
+SELECT InvoiceNo, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country
+FROM `retail-analytics-494014.raw_data.online_retail_raw`
+WHERE CustomerID IS NOT NULL AND Quantity > 0 AND UnitPrice > 0;
---
+++ sql/insights.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE TABLE `retail-analytics-494014.raw_data.country_sales_performance` AS
+WITH country_stats AS (
+  SELECT Country, CAST(ROUND(SUM(Quantity * UnitPrice), 2) AS NUMERIC) AS TotalRevenue, COUNT(DISTINCT InvoiceNo) AS Transaction_Count, COUNT(DISTINCT CustomerID) AS Customer_Count
+  FROM `retail-analytics-494014.raw_data.online_retail_cleaned`
+  GROUP BY 1
+)
+SELECT *, CAST(ROUND(TotalRevenue / Transaction_Count, 2) AS NUMERIC) AS AvgOrderValue, CAST(ROUND(TotalRevenue / Customer_Count, 2) AS NUMERIC) AS AvgSpendPerCustomer
+FROM country_stats
+WHERE Customer_Count > 0
+ORDER BY TotalRevenue DESC;
